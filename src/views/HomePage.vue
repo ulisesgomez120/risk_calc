0273eccf6a148964a3337d872d4c63e8b8684ce8
Ulises Gomez <ugomez@Justins-MacBook-Pro.local>
2022-09-26 12:53:37 -0700

    major changes, ui, data, methods

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,10 @@
 <template>
   <ion-page> 
-
-    <ion-content :fullscreen="true">  
+    <ion-content :fullscreen="true"> 
+      <div id="logo_container">
+        <ion-img src="assets/icon/Logo.png"></ion-img> 
+      </div>
+      <ion-button @click="show_settings" id="settings_btn" size="small" fill="clear"><ion-icon slot="start" name="settings-outline"></ion-icon>Settings</ion-button>
       <div id="container">
         <!-- form -->
         <ion-item class="price_inputs">
@@ -25,7 +28,8 @@
             <ion-label>Short</ion-label>
           </ion-segment-button>
         </ion-segment>
-        <div class="instructions" v-if="risk <= 0">
+        <!-- just to show risk for now -->
+        <div class="instructions" v-if="risk < 0">
           <ion-item>
             <ion-label class="ion-text-wrap">Enter an entry and stop loss to generate exits</ion-label>
           </ion-item>
@@ -34,9 +38,21 @@
           </ion-item>
         </div>
         <div class="generate_exits" v-else>
-            <ion-item>
+          <div class="calculated_data">
+            <p>Risk Per Share</p>
+            <p>{{risk}}</p>
+          </div>
+          <div class="calculated_data">
+            <p>Cost To Enter</p>
+            <p>{{cost_to_enter.toLocaleString()}}</p>
+          </div>
+          <div class="calculated_data">
+            <p>Shares</p>
+            <p>{{max_shares}}</p>
+          </div>
+            <!-- <ion-item>
               <ion-label class="calculated_data">
-                Risk Per Share
+                Risk Per Share {{risk}}
               </ion-label>
               <ion-label class="calculated_data">
                 Cost To Enter
@@ -44,15 +60,16 @@
               <ion-label class="calculated_data">
                 Shares
               </ion-label>
-            </ion-item>
+            </ion-item> -->
         </div>
+
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonPage, IonLabel, IonInput, IonItem, IonSegment, IonSegmentButton, } from '@ionic/vue';
+import { IonContent, IonPage, IonLabel, IonInput, IonItem, IonSegment, IonSegmentButton, IonImg, IonIcon, IonButton } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -65,6 +82,9 @@ export default defineComponent({
     IonLabel,
     IonSegment, 
     IonSegmentButton,
+    IonImg,
+    IonIcon,
+    IonButton
   },
   data() {
     return {
@@ -72,38 +92,49 @@ export default defineComponent({
       stop_loss: '',
       manual_exit: '',
       direction: 'long',
-      risk: 0
+      max_shares: 0,
+      cost_to_enter: 0,
+      settings: {
+        account_balance: 1400,
+        max_risk: {
+          percentage: .05,
+          dollar_amount: 0
+        },
+        max_position_dollar_amount: 300,
+        calc_cost_to_enter_using: 'mp', //mp = max_position, mr = max_risk
+      },
+      //also affect visuals
+      risk: 0,
+      show_exits: false,
+    }
+  },watch: {
+    risk(new_val) {
+      if (new_val > 0) {
+        // generate shares, cost to enter function 
+        let entry = this.entry == '' ? 0 : Number(this.entry);
+        if (this.settings.calc_cost_to_enter_using === 'mr') {
+          if (this.settings.max_risk.dollar_amount > 0) {
+            this.max_shares = Math.floor(this.settings.max_risk.dollar_amount / new_val);
+            let entry = this.entry == '' ? 0 : Number(this.entry);
+            this.cost_to_enter = this.max_shares * entry;
+          } else if (this.settings.max_risk.percentage > 0) {
+            let dollars = Math.floor(this.settings.account_balance * this.settings.max_risk.percentage);
+            console.log(dollars);
+            this.max_shares = Math.floor(dollars / new_val);
+            let entry = this.entry == '' ? 0 : Number(this.entry);
+            this.cost_to_enter = this.max_shares * entry;
+          }
+        } else {
+          this.max_shares = this.settings.max_position_dollar_amount / entry;
+          this.cost_to_enter = this.max_shares * entry;
+        }
+        // generate exits
+        //static 3x, 5x
+        //generate list and call generateExits 
+        
+      }
     }
   },
-  // watch: {
-  //   // entry(new_val) {
-  //   //   let risk = 0;
-  //   //   let stop_loss = this.stop_loss === '' ? 0 : this.stop_loss;
-  //   //   console.log(new_val);
-  //   //   if (new_val > 0 && stop_loss > 0) {
-  //   //     if (this.direction === 'long') {
-  //   //       risk = +(new_val) - +(stop_loss)
-  //   //     } else {
-  //   //       risk = +(stop_loss) - +(new_val)
-  //   //     }
-  //   //   }
-  //   //   this.risk = risk;
-  //   //   console.log(this.risk)
-  //   // },
-  //   // stop_loss(new_val) {
-  //   //   let risk = 0;
-  //   //   let entry = this.entry === '' ? 0 : this.entry;
-  //   //   console.log(new_val);
-  //   //   if (entry > 0 && new_val > 0) {
-  //   //     if (this.direction === 'long') {
-  //   //       risk = +(entry) - +(new_val)
-  //   //     } else {
-  //   //       risk = +(new_val) - +(entry)
-  //   //     }
-  //   //   }
-  //   //   this.risk = risk;
-  //   // }
-  // },
   methods: {
     segmentChanged (e:any) {
       // use for icon change, rotate on change
@@ -113,26 +144,48 @@ export default defineComponent({
       let risk = 0;
       let stop_loss = this.stop_loss === '' ? 0 : this.stop_loss;
       let entry = this.entry === '' ? 0 : this.entry;
-      console.log(stop_loss, entry);
       if (entry > 0 && stop_loss > 0) {
         if (this.direction === 'long') {
-          risk = +(entry) - +(stop_loss)
+          risk = Number((Number(entry) - Number(stop_loss)).toFixed(2));
         } else {
-          risk = +(stop_loss) - +(entry)
+          risk = Number((Number(stop_loss) - Number(entry)).toFixed(2));
         }
       }
       this.risk = risk;
+    },
+    show_settings() {
+      //
+      console.log('settings')
     }
 
   }
 });
 
+// function generateExits(ratio: number, risk: number, entry: any, shares: number) {
+//   //for list that calls this, loop through settings.exits[] and grab the rest from data.
+//   // if manual input for exit then add to front of settings.exits[]
+//   let reward = Number((risk * ratio).toFixed(2));
+//   console.log(reward);
+//   let exit = entry + reward;
+//   console.log(exit)
+//   let potential = reward * shares;
+//   console.log(potential)
+// }
+
 </script>
 
 <style scoped>
+#logo_container {
+  height: 65%;
+}
+#settings_btn {
+  position: absolute;
+  right: 0;
+  top: 45px;
+  color: lightgray;
+}
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
@@ -157,8 +210,19 @@ export default defineComponent({
 #container a {
   text-decoration: none;
 }
+.generate_exits {
+  display: flex;
+  justify-content: space-around;
+}
 .calculated_data {
   text-align: center;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+}
+.calculated_data:nth-of-type(2) {
+  border-left: 1px solid gray;
+  border-right: 1px solid gray;
 }
 .price_inputs {
   margin-bottom: 12px;
